465cf0b49c5e55c0454ee788f4f3b9d2290194b3
Eroslanov <166691699+Eroslanov@users.noreply.github.com>
2024-10-11 19:30:12 +0300

    Update mainClass.php

--- src/mainClass.php
+++ src/mainClass.php
@@ -4,6 +4,27 @@ namespace App;
 
 class CaesarCipher
 {
+    public static function arrayUnique(array $arr): array
+  {
+    return array_values(array_unique($arr));
+  }
+
+  public static function mostRecent(string $text): string
+  {
+
+    $userText = mb_strtolower($text);
+    $userText = preg_replace('/\s+/', ' ', $userText);
+    $userText = preg_replace('/[^ a-zа-яё\d]/ui', '', $userText);
+    $words = array_filter(explode(" ", $userText));
+
+    $coutWords = array_count_values($words);
+    arsort($coutWords);
+
+    $mostCommonWordCount = reset($coutWords);
+    $mostCommonWord = array_keys($coutWords, $mostCommonWordCount);
+
+    return implode(' ', $mostCommonWord);
+  }
     public static function encrypt(string $text, int $key = 6): string
     {
         $array = array(
